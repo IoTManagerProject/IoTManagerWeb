7208db0787c7126d67db58b78462ee824de7839b
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2021-08-18 05:13:27 +0800

    last

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 module.exports = {
   purge: {
     enabled: !process.env.ROLLUP_WATCH,
+    //mode: 'all',
+    //preserveHtmlElements: false,
     content: ['./public/index.html', './src/**/*.svelte'],
     options: {
       defaultExtractor: content => [
         ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
         ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
       ],
+      //keyframes: true,
     },
   },
   darkMode: false, // or 'media' or 'class'
