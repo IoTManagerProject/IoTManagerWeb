64cee1cedf8c670c6937bc728c632dabbae08af5
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2021-10-18 02:41:05 +0800

    working version

--- src/widgets/Input.svelte
+++ src/widgets/Input.svelte
@@ -4,6 +4,7 @@
 
 <script>
   export let widget;
+  export let value;
 </script>
 
 <div class="card-items">
@@ -12,18 +13,17 @@
     <label class="widget-descr-gray-left">{!widget.descr ? "" : widget.descr}</label>
   </div>
   <div class="md:w-1/3 lg:w-1/3 2xl:w-1/3">
-    <!-- on:change={WSpush(ws, topic, value)} -->
     {#if widget.type == "number"}
-      <input class="widget-input-indigo widget-input-red={widget['send'] == true} text-right" step="0.1" on:change={((widget["send"] = true), WSpush(widget.ws, widget.topic, widget.status))} bind:value={widget.status} type="number" />
+      <input on:change={WSpush(widget.ws, widget.topic, widget.status)} bind:value class="widget-input-indigo text-right" step="0.1" type="number" />
     {/if}
     {#if widget.type == "text"}
-      <input class="widget-input-indigo text-right" on:change={((widget["send"] = true), WSpush(widget.ws, widget.topic, widget.status))} bind:value={widget.status} type="text" />
+      <input on:change={WSpush(widget.ws, widget.topic, widget.status)} bind:value class="widget-input-indigo text-right" type="text" />
     {/if}
     {#if widget.type == "date"}
-      <input class="widget-input-indigo text-right" on:change={((widget["send"] = true), WSpush(widget.ws, widget.topic, widget.status))} bind:value={widget.status} type="date" />
+      <input on:change={WSpush(widget.ws, widget.topic, widget.status)} bind:value class="widget-input-indigo text-right" type="date" />
     {/if}
     {#if widget.type == "time"}
-      <input class="widget-input-indigo text-right" on:change={((widget["send"] = true), WSpush(widget.ws, widget.topic, widget.status))} bind:value={widget.status} type="time" />
+      <input on:change={WSpush(widget.ws, widget.topic, widget.status)} bind:value class="widget-input-indigo text-right" type="time" />
     {/if}
   </div>
 </div>
